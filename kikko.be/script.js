43b84d1a4d34d81acc7f5efe7b908c25f7b05325
Kurodo Henro Onsen <71895117+kurodohenroonsen@users.noreply.github.com>
2025-08-07 01:27:37 +0200

    a

---
+++ kikko.be/script.js
@@ -0,0 +1,99 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const canvas = document.getElementById('particle-canvas');
+    const ctx = canvas.getContext('2d');
+
+    let particlesArray;
+
+    // --- Configuration ---
+    const particleColor = '#FFC300'; // --kikko-honey-yellow
+    const lineColor = 'rgba(69, 182, 156, 0.5)'; // --kikko-cyan-glow with alpha
+    const particleSpeed = 0.5;
+    const connectDistance = 150;
+
+    // --- Redimensionnement du Canvas ---
+    function resizeCanvas() {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+    }
+    window.addEventListener('resize', resizeCanvas);
+    resizeCanvas();
+
+    // --- Classe Particule ---
+    class Particle {
+        constructor(x, y, directionX, directionY, size, color) {
+            this.x = x;
+            this.y = y;
+            this.directionX = directionX;
+            this.directionY = directionY;
+            this.size = size;
+            this.color = color;
+        }
+
+        draw() {
+            ctx.beginPath();
+            ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2, false);
+            ctx.fillStyle = this.color;
+            ctx.fill();
+        }
+
+        update() {
+            if (this.x > canvas.width || this.x < 0) {
+                this.directionX = -this.directionX;
+            }
+            if (this.y > canvas.height || this.y < 0) {
+                this.directionY = -this.directionY;
+            }
+            this.x += this.directionX;
+            this.y += this.directionY;
+            this.draw();
+        }
+    }
+
+    // --- Création des Particules ---
+    function init() {
+        particlesArray = [];
+        let numberOfParticles = (canvas.height * canvas.width) / 9000;
+        for (let i = 0; i < numberOfParticles; i++) {
+            let size = (Math.random() * 2) + 1;
+            let x = (Math.random() * ((innerWidth - size * 2) - (size * 2)) + size * 2);
+            let y = (Math.random() * ((innerHeight - size * 2) - (size * 2)) + size * 2);
+            let directionX = (Math.random() * 2 - 1) * particleSpeed;
+            let directionY = (Math.random() * 2 - 1) * particleSpeed;
+            particlesArray.push(new Particle(x, y, directionX, directionY, size, particleColor));
+        }
+    }
+
+    // --- Connexion des Particules ---
+    function connect() {
+        let opacityValue = 1;
+        for (let a = 0; a < particlesArray.length; a++) {
+            for (let b = a; b < particlesArray.length; b++) {
+                let distance = ((particlesArray[a].x - particlesArray[b].x) * (particlesArray[a].x - particlesArray[b].x))
+                             + ((particlesArray[a].y - particlesArray[b].y) * (particlesArray[a].y - particlesArray[b].y));
+                if (distance < (connectDistance * connectDistance)) {
+                    opacityValue = 1 - (distance / (connectDistance * connectDistance));
+                    ctx.strokeStyle = lineColor.replace('0.5', opacityValue.toString());
+                    ctx.lineWidth = 1;
+                    ctx.beginPath();
+                    ctx.moveTo(particlesArray[a].x, particlesArray[a].y);
+                    ctx.lineTo(particlesArray[b].x, particlesArray[b].y);
+                    ctx.stroke();
+                }
+            }
+        }
+    }
+
+    // --- Boucle d'Animation ---
+    function animate() {
+        requestAnimationFrame(animate);
+        ctx.clearRect(0, 0, innerWidth, innerHeight);
+
+        for (let i = 0; i < particlesArray.length; i++) {
+            particlesArray[i].update();
+        }
+        connect();
+    }
+
+    init();
+    animate();
+});
